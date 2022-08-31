54ea2cb674cc42fd99e628342ff87f822675510b
Tanya <veneficav@list.ru>
2022-08-31 16:25:32 +0700

    create

---
+++ Example5_if/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Please write username:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "tanya")
+{
+    Console.WriteLine("I love you, Tanya!");
+}
+else
+{
+    Console.Write("Hello! ");
+    Console.WriteLine(username);
+}
+
