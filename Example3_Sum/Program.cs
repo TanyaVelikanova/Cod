5d173c8200e32f96648c10c56aa068c52a130413
Tanya <veneficav@list.ru>
2022-08-31 15:22:01 +0700

    add random

--- Example3_Sum/Program.cs
+++ Example3_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 34;
-int mumberB = 56;
-int result = numberA + mumberB;
+﻿int numberA = new Random().Next(1,10); // случайное исло в диапазоне от 1 до 9 включительно
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10); 
+Console.WriteLine(numberB);
+int result = numberA + numberB;
 Console.WriteLine(result);
